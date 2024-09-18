38ad06638be4bcfb0232256e924a62ef7f17484d
Andres_Prado_Morgaz <andres.prado.external@eviden.com>
2024-09-18 20:11:27 +0200

    Added a toggler for the projects list to collapse it to 2 projects or expand to all

--- script.js
+++ script.js
@@ -41,6 +41,14 @@ function loadTheme() {
 const lightToggler = document.getElementById('lightModeToggler')
 lightToggler.addEventListener('click', handleLightToggler)
 
+const togglableProjects = document.querySelectorAll('#projectsList > *:nth-child(n+3)')
+const projectsListTogglers = document.querySelectorAll('.toggleProjects')
+projectsListTogglers.forEach(toggler => toggler.addEventListener('click', toggleProjectsList))
+
+function toggleProjectsList() {
+    togglableProjects.forEach(element => element.classList.toggle('hidden'))
+}
+
 loadTheme()
 
 let lastScrollTop = 0;
