812553a750a551147eaf25e1d6efdb193a9b4d56
Andres_Prado_Morgaz <andres.prado.external@eviden.com>
2024-09-22 18:47:22 +0200

    Little refactor color mode handling, functionality unaltered

--- toggleColorMode.js
+++ toggleColorMode.js
@@ -1,54 +1,49 @@
 // Color mode handling:
-const lightModeTogglerIcon = document.getElementById('lightModeTogglerIcon')
+const colorModeTogglerIcon = document.getElementById('lightModeTogglerIcon')
 const goDarkTexts = document.querySelectorAll('.showInLightMode')
 const goLightTexts = document.querySelectorAll('.showInDarkMode')
 
-const handleLightToggle = () => {
-    // Check the current theme, set the new theme to be the other option, update it in :root (document.documentElement) and store it in localStorage:
-    const currentTheme = document.documentElement.getAttribute('color-mode')
-    const newTheme = currentTheme === 'dark' ? 'light' : 'dark'
-    document.documentElement.setAttribute('color-mode', newTheme)
-    localStorage.setItem('theme', newTheme)
+const setColorMode = (theme) => {
+    document.documentElement.setAttribute('color-mode', theme)
 
     // Change the icon:
-    if (newTheme === 'dark') {
-        lightModeTogglerIcon.classList.add('bi-brightness-high-fill')
-        lightModeTogglerIcon.classList.remove('bi-moon-fill')
+    if (theme === 'dark') {
+        colorModeTogglerIcon.classList.add('bi-brightness-high-fill')
+        colorModeTogglerIcon.classList.remove('bi-moon-fill')
         goDarkTexts.forEach(element => element.classList.add('hidden'))
         goLightTexts.forEach(element => element.classList.remove('hidden'))
     } else {
-        lightModeTogglerIcon.classList.add('bi-moon-fill')
-        lightModeTogglerIcon.classList.remove('bi-brightness-high-fill')
+        colorModeTogglerIcon.classList.add('bi-moon-fill')
+        colorModeTogglerIcon.classList.remove('bi-brightness-high-fill')
         goLightTexts.forEach(element => element.classList.add('hidden'))
         goDarkTexts.forEach(element => element.classList.remove('hidden'))
     }
 }
 
+// Change current color theme, update it in :root (document.documentElement) and store it in localStorage:
+const handleLightToggle = () => {
+    const currentTheme = document.documentElement.getAttribute('color-mode')
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark'
+    localStorage.setItem('theme', newTheme)
+
+    setColorMode(newTheme)
+}
+
 // Check the user's color mode preference on page load:
 function loadTheme() {
     const savedTheme = localStorage.getItem('theme')
+    const isDarkPrefered = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
     let theme
 
     if (savedTheme) {
         theme = savedTheme
-    } else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    } else if (isDarkPrefered) {
         theme = 'dark'
     } else {
         theme = 'light'
     }
-    document.documentElement.setAttribute('color-mode', theme)
 
-    if (theme === 'dark') {
-        lightModeTogglerIcon.classList.remove('bi-moon-fill')
-        lightModeTogglerIcon.classList.add('bi-brightness-high-fill')
-        goDarkTexts.forEach(element => element.classList.add('hidden'))
-        goLightTexts.forEach(element => element.classList.remove('hidden'))
-    } else {
-        lightModeTogglerIcon.classList.remove('bi-brightness-high-fill')
-        lightModeTogglerIcon.classList.add('bi-moon-fill')
-        goLightTexts.forEach(element => element.classList.add('hidden'))
-        goDarkTexts.forEach(element => element.classList.remove('hidden'))
-    }
+    setColorMode(theme)
 }
 
 const lightToggler = document.getElementById('lightModeToggler')
