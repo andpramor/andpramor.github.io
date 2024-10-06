5d0290a9e45d9d1ae2d060cb65e9cd7dbfb87c74
Andres Prado Morgaz <andpramordev@gmail.com>
2024-10-06 21:43:06 +0200

    Added prettier config and some fade in subtle animations

---
+++ fadeIns.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const about = document.getElementById('aboutSection')
+  about.style.opacity = 0
+
+  const observer = new IntersectionObserver(
+    (observedItems) => {
+      observedItems.forEach((item) => {
+        if (item.isIntersecting) {
+          item.target.style.animation =
+            'fadeInLeft 2s ease 0s 1 normal forwards'
+          observer.unobserve(item.target) // Stop observing so the animation isn't retriggered
+        }
+      })
+    },
+    {
+      threshold: 0.3 // % element visibility before the observer callback triggers
+    }
+  )
+
+  observer.observe(about)
+})
