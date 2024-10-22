c4da8477479d2291691c9cd2dd767621e5504705
Andres Prado Morgaz <andpramordev@gmail.com>
2024-10-22 18:18:40 +0200

    Trying to redirect to every single proyect

--- 404.js
+++ 404.js
@@ -1,13 +1,22 @@
-let countdown = 5
+let path = window.location.pathname
 
-const timer = document.getElementById('countdownTimer')
+// Comprobar si está dentro de un proyecto, en "miurl/proyectoX/"
+let match = path.match(/^\/([^\/]+)\/([^\/]+)\/?/)
 
-const interval = setInterval(() => {
+if (match) {
+  let projectName = match[1] // "proyecto1", "proyecto2", etc.
+  window.location.replace('https://andrespradomorgaz.com/' + projectName + '/')
+} else {
+  let countdown = 5
+  const timer = document.getElementById('countdownTimer')
+
+  const interval = setInterval(() => {
     countdown--
     timer.textContent = countdown
 
     if (countdown === 0) {
-        clearInterval(interval)
-        window.location.href = 'https://andrespradomorgaz.com'
+      clearInterval(interval)
+      window.location.href = 'https://andrespradomorgaz.com'
     }
-}, 1000)
+  }, 1000)
+}
