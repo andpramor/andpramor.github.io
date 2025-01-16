67ff6a8c1e10583fd834930b33dd231e2a4a74b8
Andres Prado Morgaz <andpramordev@gmail.com>
2025-01-16 12:37:21 +0100

    Got rid of Bootstrap icons in favor of svgs

--- toggleColorMode.js
+++ toggleColorMode.js
@@ -1,49 +1,53 @@
 // Color mode handling:
-const colorModeTogglerIcon = document.getElementById('lightModeTogglerIcon')
+const colorModeTogglerPath = document.getElementById('lightModeTogglerPath')
 const goDarkTexts = document.querySelectorAll('.showInLightMode')
 const goLightTexts = document.querySelectorAll('.showInDarkMode')
+const sunPath =
+  'M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708'
+const moonPath =
+  'M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278'
 
 const setColorMode = (theme) => {
-    document.documentElement.setAttribute('color-mode', theme)
+  document.documentElement.setAttribute('color-mode', theme)
 
-    // Change the icon:
-    if (theme === 'dark') {
-        colorModeTogglerIcon.classList.add('bi-brightness-high-fill')
-        colorModeTogglerIcon.classList.remove('bi-moon-fill')
-        goDarkTexts.forEach(element => element.classList.add('hidden'))
-        goLightTexts.forEach(element => element.classList.remove('hidden'))
-    } else {
-        colorModeTogglerIcon.classList.add('bi-moon-fill')
-        colorModeTogglerIcon.classList.remove('bi-brightness-high-fill')
-        goLightTexts.forEach(element => element.classList.add('hidden'))
-        goDarkTexts.forEach(element => element.classList.remove('hidden'))
-    }
+  // Change the icon:
+  if (theme === 'dark') {
+    colorModeTogglerPath.setAttribute('d', moonPath)
+    goDarkTexts.forEach((element) => element.classList.add('hidden'))
+    goLightTexts.forEach((element) => element.classList.remove('hidden'))
+  } else {
+    colorModeTogglerPath.setAttribute('d', sunPath)
+    goLightTexts.forEach((element) => element.classList.add('hidden'))
+    goDarkTexts.forEach((element) => element.classList.remove('hidden'))
+  }
 }
 
 // Change current color theme, update it in :root (document.documentElement) and store it in localStorage:
 const handleLightToggle = () => {
-    const currentTheme = document.documentElement.getAttribute('color-mode')
-    const newTheme = currentTheme === 'dark' ? 'light' : 'dark'
-    localStorage.setItem('theme', newTheme)
+  const currentTheme = document.documentElement.getAttribute('color-mode')
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark'
+  localStorage.setItem('theme', newTheme)
 
-    setColorMode(newTheme)
+  setColorMode(newTheme)
 }
 
 // Check the user's color mode preference on page load:
 function loadTheme() {
-    const savedTheme = localStorage.getItem('theme')
-    const isDarkPrefered = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
-    let theme
+  const savedTheme = localStorage.getItem('theme')
+  const isDarkPrefered =
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches
+  let theme
 
-    if (savedTheme) {
-        theme = savedTheme
-    } else if (isDarkPrefered) {
-        theme = 'dark'
-    } else {
-        theme = 'light'
-    }
+  if (savedTheme) {
+    theme = savedTheme
+  } else if (isDarkPrefered) {
+    theme = 'dark'
+  } else {
+    theme = 'light'
+  }
 
-    setColorMode(theme)
+  setColorMode(theme)
 }
 
 const lightToggler = document.getElementById('lightModeToggler')
